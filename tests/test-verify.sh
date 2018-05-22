146f1210a9bac5c44aa7efda63ddb9c3506315e0
Adam Dyson <adam.dyson@outlook.com>
2018-05-22 18:53:01 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
 # Verify the installed location.
 docker exec --tty ${container_id} env TERM=xterm which aws
-
 # Verify the installed version.
 docker exec --tty ${container_id} env TERM=xterm aws --version
-
 # Verify the file contents.
 docker exec --tty ${container_id} env TERM=xterm cat '/home/jack/.aws/config'
 docker exec --tty ${container_id} env TERM=xterm cat '/home/jill/.aws/config'
